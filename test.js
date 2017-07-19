91d443c54a122e9de307b28c866d5a39147167ae
Ian Jennings <ian@meetjennings.com>
2017-07-19 16:04:51 -0700

    merge new docs in

--- test.js
+++ test.js
@@ -1,33 +1,13 @@
-"use strict";
+ "use strict";
 const assert = require('chai').assert;
 
-const typingIndicator = require('./plugins/typingIndicator.js');
-const append = require('./plugins/append.js');
-const messageHistory = require('./plugins/messageHistory.js');
+const typingIndicator = require('ocf-typing-indicator');
+// const append = require('ocf-append');
+const messageHistory = require('ocf-history');
 
 const OpenChatFramework = require('./src/index.js');
 
 let agentInput = process.env.AGENT || 'pubnub';
-
-const agents = {
-    pubnub: {
-        service: 'pubnub',
-            config: {
-            publishKey: 'demo',
-            subscribeKey: 'demo',
-            uuid: new Date(),
-            state: {}
-        }
-    },
-    socketio: {
-        service: 'socketio',
-        config: {
-            endpoint: 'http://localhost:8000',
-            uuid: new Date(),
-            state: {}
-        }
-    }
-};
 
 describe('import', function() {
 
@@ -48,9 +28,9 @@ describe('config', function() {
     it('should be configured', function() {
 
         OCF = OpenChatFramework.create({
-            globalChannel: 'test-channel',
-            rltm: agents[agentInput]
-        });
+            publishKey: 'demo',
+            subscribeKey: 'demo'
+        }, 'test-channel');
 
         assert.isOk(OCF);
 
@@ -82,7 +62,7 @@ describe('chat', function() {
 
     it('should get ready callback', function(done) {
 
-        chat.ready(() => {
+        chat.on('$ocf.ready', () => {
 
             done();
 
@@ -99,7 +79,7 @@ describe('chat', function() {
 
         });
 
-        chat.send('something', {
+        chat.emit('something', {
             text: 'hello world'
         });
 
@@ -107,68 +87,6 @@ describe('chat', function() {
 
 });
 
-let pluginchat;
-
-describe('plugins', function() {
-
-    it('should be created', function() {
-
-        pluginchat = new OCF.Chat('pluginchat' + new Date().getTime());
-
-        pluginchat.plugin(typingIndicator({
-            timeout: 5000
-        }));
-
-        pluginchat.plugin(append({
-            send: pub_append,
-            broadcast: sub_append
-        }));
-
-    });
-
-    it('publish and subscribe hooks should be called', function(done) {
-
-        pluginchat.ready(() => {
-
-            pluginchat.on('message', (payload) => {
-
-                assert.isObject(payload);
-                assert.isAbove(payload.data.text.indexOf(pub_append), 0, 'publish hook executed');
-                assert.isAbove(payload.data.text.indexOf(sub_append), 0, 'subscribe hook executed');
-                assert.isAbove(payload.data.text.indexOf(sub_append), payload.data.text.indexOf(pub_append), 'subscribe hook was called before publish hook');
-                done();
-
-            });
-
-            pluginchat.send('message', {
-                text: 'hello world'
-            });
-
-        });
-
-    });
-
-    it('typing indicator', function(done) {
-
-        pluginchat.once('$typingIndicator.startTyping', () => {
-            done();
-        });
-
-        pluginchat.typingIndicator.startTyping();
-
-    });
-
-    it('wildcard event', function(done) {
-
-        pluginchat.once('$typingIndicator.*', () => {
-            done();
-        });
-
-        pluginchat.typingIndicator.startTyping();
-
-    });
-
-});
 
 let historyChan = 'history-chat-4';
 
@@ -176,24 +94,27 @@ describe('history plugin', function() {
 
     it('should be created', function(done) {
 
-
         let historychat = new OCF.Chat(historyChan);
+        let ready = false;
 
-        historychat.ready(() => {
+        historychat.on('$ocf.ready', () => {
 
-            historychat.send('message', {
+            historychat.emit('message', {
                 text: 'hello world'
             });
 
-            historychat.send('message', {
+            historychat.emit('message', {
                 text: 'hello world'
             });
 
-            historychat.send('message', {
+            historychat.emit('message', {
                 text: 'hello world'
             });
 
-            done();
+            if(!ready) {
+                done();
+                ready = true;
+            }
 
         })
 
@@ -204,16 +125,125 @@ describe('history plugin', function() {
         this.timeout(10000);
 
         let historychat2 = new OCF.Chat(historyChan);
-        historychat2.plugin(messageHistory())
 
-        let responded = false;
+        historychat2.history('message');
 
         historychat2.once("$history.message", (message) => {
-
+            assert.isOk(message);
             done();
-
         });
 
     });
 
 });
+
+
+// let pluginchat;
+
+// describe('plugins', function() {
+
+//     it('should be created', function() {
+
+//         pluginchat = new OCF.Chat('pluginchat' + new Date().getTime());
+
+//         pluginchat.plugin(typingIndicator({
+//             timeout: 5000
+//         }));
+
+//         pluginchat.plugin(append({
+//             send: pub_append,
+//             broadcast: sub_append
+//         }));
+
+//     });
+
+//     it('publish and subscribe hooks should be called', function(done) {
+
+//         pluginchat.ready(() => {
+
+//             pluginchat.on('message', (payload) => {
+
+//                 assert.isObject(payload);
+//                 assert.isAbove(payload.data.text.indexOf(pub_append), 0, 'publish hook executed');
+//                 assert.isAbove(payload.data.text.indexOf(sub_append), 0, 'subscribe hook executed');
+//                 assert.isAbove(payload.data.text.indexOf(sub_append), payload.data.text.indexOf(pub_append), 'subscribe hook was called before publish hook');
+//                 done();
+
+//             });
+
+//             pluginchat.send('message', {
+//                 text: 'hello world'
+//             });
+
+//         });
+
+//     });
+
+//     it('typing indicator', function(done) {
+
+//         pluginchat.once('$typingIndicator.startTyping', () => {
+//             done();
+//         });
+
+//         pluginchat.typingIndicator.startTyping();
+
+//     });
+
+//     it('wildcard event', function(done) {
+
+//         pluginchat.once('$typingIndicator.*', () => {
+//             done();
+//         });
+
+//         pluginchat.typingIndicator.startTyping();
+
+//     });
+
+// });
+
+// let historyChan = 'history-chat-4';
+
+// describe('history plugin', function() {
+
+//     it('should be created', function(done) {
+
+//         let historychat = new OCF.Chat(historyChan);
+
+//         historychat.on('$ocf.ready', () => {
+
+//             historychat.send('message', {
+//                 text: 'hello world'
+//             });
+
+//             historychat.send('message', {
+//                 text: 'hello world'
+//             });
+
+//             historychat.send('message', {
+//                 text: 'hello world'
+//             });
+
+//             done();
+
+//         })
+
+//     });
+
+//     it('history', function(done) {
+
+//         this.timeout(10000);
+
+//         let historychat2 = new OCF.Chat(historyChan);
+//         historychat2.plugin(messageHistory())
+
+//         let responded = false;
+
+//         historychat2.once("$history.message", (message) => {
+
+//             done();
+
+//         });
+
+//     });
+
+// });
